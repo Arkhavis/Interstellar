4059ba95a8695706b8d791d269e32588554c4134
Kayden <117483970+Arkhavis@users.noreply.github.com>
2025-02-24 18:35:32 -0600

    Massive Update

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Cylis: "Cylis164341",
+    Carter: "Pizza",
+    Jayden: "KingBunny",
+    Evan: "164093ea",
+    Owner: "4523",
+    Levi: "Evergreen85",
+    Mason: "Hasard26!",
+    River: "Something3655",
+    Sylis: "Therealguy",
   },
 };
 
