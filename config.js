843028e11573333b563e92e5dcc57908be24df3f
Kayden <117483970+DarkTurnsOfficial@users.noreply.github.com>
2025-03-07 00:51:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
+    Kena: "164153",
     Cylis: "Cylis164341",
     Carter: "Pizza",
     Owner: "4523",
